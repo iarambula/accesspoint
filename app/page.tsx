7325144355eaf5692d5fd9109baef7ce76882fe2
Ivan Arambula <ivan@sigoseguros.com>
2025-04-02 14:47:12 -0400

    add new elements, make random

--- app/page.tsx
+++ app/page.tsx
@@ -10,23 +10,56 @@ import {
 import { formatDistanceToNow, parseISO } from "date-fns";
 import Parser from "rss-parser";
 
+interface FeedItem extends Parser.Item {
+  feedTitle?: string;
+  feedLink?: string;
+}
+
 export default async function Home() {
   const parser = new Parser();
 
   const feeds = await Promise.all([
-    // parser.parseURL("https://peprofessional.com/feed/"),
     parser.parseURL("https://www.privateequitywire.co.uk/feed/"),
+    parser.parseURL("https://peprofessional.com/feed/"),
+    parser.parseURL("https://www.altassets.net/feed"),
   ]);
+
+  let feedItems = feeds.reduce<FeedItem[]>((acc, feed) => {
+    if (feed.items) {
+      const mergedItems = feed.items.map((item) => ({
+        ...item,
+        feedTitle: feed.title,
+        feedLink: feed.link,
+      }));
+      acc.push(...mergedItems);
+    }
+    return acc;
+  }, []);
+
+  feedItems = feedItems.sort((a, b) => {
+    const dateA = a.isoDate ? parseISO(a.isoDate).getTime() : 0;
+    const dateB = b.isoDate ? parseISO(b.isoDate).getTime() : 0;
+    return dateB - dateA;
+  });
 
   return (
     <Container>
       <Title order={1} mb="md">
         Your feed
       </Title>
-      {feeds[0].items.map((item) => (
+      {feedItems.map((item) => (
         <Card key={item.guid} shadow="sm" padding="lg" mb="md" withBorder>
           <Anchor href={item.link} size="lg" fw="bold" mb="xs" target="_blank">
             {item.title}
+          </Anchor>
+          <Anchor
+            size="sm"
+            c="dimmed"
+            mb="xs"
+            target="_blank"
+            href={item.feedLink}
+          >
+            {item.feedTitle}
           </Anchor>
           <Group mb="xs">
             {item.isoDate && (
@@ -37,7 +70,7 @@ export default async function Home() {
             {item.categories && (
               <Text size="sm">
                 {item.categories.map((category) => (
-                  <Pill key={category} mr="xs">
+                  <Pill key={category} mr="xs" mb="xs">
                     {category}
                   </Pill>
                 ))}
@@ -49,14 +82,12 @@ export default async function Home() {
               {item.contentSnippet}
             </Text>
           )}
-          <Card withBorder>
-            <Text>
-              <Text
-                variant="gradient"
-                fw="bold"
-                gradient={{ from: "blue", to: "green" }}
-              >
-                AccessPoint Intelligence
+          {Math.random() > 0.5 && (
+            <Card withBorder>
+              <Text>
+                <Text variant="gradient" fw="bold" component="span">
+                  AccessPoint Intelligence
+                </Text>
               </Text>
               <Text size="xs" c="dimmed">
                 Related to your preferences
@@ -68,10 +99,12 @@ export default async function Home() {
                 <Anchor>Goldman Sachs</Anchor>
                 <Anchor>Blackstone</Anchor>
               </Group>
-            </Text>
-          </Card>
+            </Card>
+          )}
         </Card>
       ))}
     </Container>
   );
 }
+
+export const dynamic = "force-dynamic";
